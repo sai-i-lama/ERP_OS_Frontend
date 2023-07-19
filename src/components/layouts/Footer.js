202a512cf8c4c60352c9f4f3e2bd83869fcaac6b
menoc61 <gillemomeni@gmail.com>
2023-07-19 13:07:01 +0200

    update footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -2,53 +2,49 @@ import { Col, Layout, Row } from "antd";
 import styles from "./Footer.module.css";
 
 function Footer() {
-	const { Footer: AntFooter } = Layout;
-	const year = new Date().getFullYear();
+  const { Footer: AntFooter } = Layout;
+  const year = new Date().getFullYear();
 
-	return (
-		<AntFooter className={styles.footer}>
-			<Row>
-				<Col xs={24} md={24} lg={12} className={styles.copyrightCol}>
-					<p className={styles.copyrightText}>
-						{year}{" "}
-						<a
-							href='/'
-							className='font-weight-bold'
-							target='_blank'
-							rel='noreferrer'>
-							DTA internship Team
-						</a>{" "}
-						ERM Solution
-					</p>
-				</Col>
-				<Col xs={24} md={24} lg={12}>
-					<div className={styles.footerMenu}>
-						<ul className={styles.footerList}>
-
-							<li className='nav-item'>
-								<a href='/' className='nav-link text-muted' target='/'>
-								Qui sommes-nous ?
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a href='/' className='nav-link text-muted' target='_blank'>
-									Documentation
-								</a>
-							</li>
-							<li className='nav-item'>
-								<a
-									href='/'
-									className='nav-link pe-0 text-muted'
-									target='_blank'>
-									License
-								</a>
-							</li>
-						</ul>
-					</div>
-				</Col>
-			</Row>
-		</AntFooter>
-	);
+  return (
+    <AntFooter className={styles.footer}>
+      <Row>
+        <Col xs={24} md={24} lg={12} className={styles.copyrightCol}>
+          <p className={styles.copyrightText}>
+            Powered by{" "}
+            <a
+              href="https://digit-tech-innov.com/"
+              className="font-weight-bold"
+              target="_blank"
+              rel="noreferrer"
+            >
+              DIGIT-TECH-INNOV SARL
+            </a>{" "}
+            ©{year}
+          </p>
+        </Col>
+        <Col xs={24} md={24} lg={12}>
+          <div className={styles.footerMenu}>
+            <ul className={styles.footerList}>
+              <li className="nav-item">
+                <a href="/doc" className="nav-link text-muted" target="_blank">
+                  Documentation
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="/"
+                  className="nav-link pe-0 text-muted"
+                  target="_blank"
+                >
+                  License
+                </a>
+              </li>
+            </ul>
+          </div>
+        </Col>
+      </Row>
+    </AntFooter>
+  );
 }
 
 export default Footer;
