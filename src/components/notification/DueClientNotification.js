a4cdf651292fcde155831104e2c20ed36f7aa660
menoc61 <gillemomeni@gmail.com>
2023-08-11 19:49:06 +0200

    add button for call and sms

--- src/components/notification/DueClientNotification.js
+++ src/components/notification/DueClientNotification.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { BellOutlined,WhatsAppOutlined, MessageOutlined  } from "@ant-design/icons";
-import { Alert,Button } from "antd";
+import {
+  BellOutlined,
+  MessageOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
+import { Alert, Button } from "antd";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { loadAllCustomer } from "../../redux/actions/customer/getCustomerAction";
 function DueClientNotification({ list }) {
@@ -18,6 +23,23 @@ function DueClientNotification({ list }) {
   }
   const filteredList = list ? list.filter((item) => item.due_amount > 0) : [];
 
+  // send message function
+  function sendMessage(phoneNumber) {
+    axios
+      .post("/sms", {
+        number: phoneNumber,
+        message:
+          "Cher(e) [Momeni gille],\n\nJ'espère que vous allez bien. Je me permets de vous contacter au sujet du solde en suspens concernant votre dette envers notre entreprise, [sai i lama]. Le montant dû s'élève à [montant] et la date d'échéance était fixée au [15/04/2003].\nNous vous serions reconnaissants de bien vouloir effectuer le paiement dans les plus brefs délais afin de régulariser cette situation. Vous pouvez effectuer le règlement via [presenciel].\n\nMerci d'avance pour votre attention et votre coopération.\n\nCordialement,\n[service client],\n[sai i lama],\n[693972665]",
+      })
+      .then((response) => {
+        // Handle success, if needed
+        console.log("Message sent successfully");
+      })
+      .catch((error) => {
+        // Handle error, if needed
+        console.error("Failed to send message:", error);
+      });
+  }
   return (
     <div className="notification-icon-container">
       <div>
@@ -49,8 +71,23 @@ function DueClientNotification({ list }) {
                       </Link>{" "}
                       a une dette de {item?.due_amount}. depuis le{" "}
                       {moment(item.updated_at).format("DD/MM/YYYY")} contact:{" "}
-                      <a href={`tel:${customer?.phone}`}>{customer?.phone}</a>
+                      <Button type="primary" icon={<PhoneOutlined />} style={{marginBottom: "2px"}}>
+                        <a
+                          href={`tel:${customer?.phone}`}
+                          style={{ color: "white" }}
+                        >
+                          {customer?.phone}
+                        </a>
+                      </Button>
+                      
                     </span>
+                    <Button
+                      type="primary"
+                      icon={<MessageOutlined />}
+                      onClick={() => sendMessage(customer?.phone)}
+                    >
+                      Send Message
+                    </Button>
                   </>
                 }
                 type="warning"
